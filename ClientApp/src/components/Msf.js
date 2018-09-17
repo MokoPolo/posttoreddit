bb25a131a1bb269493c6f3977443dcda08a756fa
Marc <marc.cueto@gmail.com>
2018-09-17 01:03:21 -0400

    Can finally authenticate and get token

--- ClientApp/src/components/Msf.js
+++ ClientApp/src/components/Msf.js
@@ -1,31 +1,47 @@
 import React, { Component } from 'react';
-import * as qs from 'query-string';
+import queryString from 'querystring';
 
 export class Msf extends Component {
-    displayName = Msf.name
-
+    displayName = Msf.name;
 
     async componentDidMount() {
-        console.log(location.search);
-        const parsed = qs.parse(location.search);
+        console.log(this.props.location.search);
+        const parsed = queryString.parse(this.props.location.search);
         console.log(parsed);
 
         if (parsed !== null){
-        try {
-            const response = await fetch(`https://www.reddit.com/api/v1/access_token`);
+         try {
+                const code = parsed.code;
+                const authCode = "authorization_code";
+                const data = {
+                    grant_type: authCode,
+                    code: code,
+                    redirect_uri: 'http://www.hackandslash.net/msf'
+            };
+            const settings = {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    Authorization: 'Basic cWFCd1JfLWtnb2NQcUE6U2xWN3RHYXJ4MlA0NXE2SFNNLXBNRm4yc0ZZ'
+                }
+            };
+            console.log("going in");
+            const response = await fetch(`https://www.reddit.com/api/v1/access_token`, settings);
             if (!response.ok){
                 throw Error(response.statusText);
             }
             const json = await response.json();
+             console.log("showing response.json");
+             console.log(json);
             this.setState({ data: json });
         }
-        catch (error){
+         catch (error) {
+             console.log("error");
             console.log(error);
         }
     }
-    }   
+  }   
     render() {
-        console.log(this.props.location);
       return (
         <div>
           
